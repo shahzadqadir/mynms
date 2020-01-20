c355d1d3fc4c6b28494171154b96407faed651f3
shahzadqadir <send2shahzad@gmail.com>
2020-01-20 03:06:12 +0000

    junos l3 int added

--- ConfigInterface.py
+++ ConfigInterface.py
@@ -5,9 +5,11 @@ import sys
 #from ansible.modules.network import interface
 from time import sleep
 
-intf, _ = loadUiType("l2_interface_config.ui")
+l2intf, _ = loadUiType("l2_interface_config.ui")
+l3intf, _ = loadUiType("l3_interface_config.ui")
+l3intfjunos, _ = loadUiType("l3_intf_config_junos.ui")
 
-class L2IntfConfig(QWidget,  intf):
+class L2IntfConfig(QWidget,  l2intf):
     def __init__(self, ip, username, password):
         QWidget.__init__(self)
         self.setupUi(self)
@@ -126,17 +128,227 @@ class L2IntfConfig(QWidget,  intf):
     def close_intf_dialog(self):
         self.close()
 
- 
-# def main():
-#     app = QApplication(sys.argv)
-#         
-#     myapp = L2IntfConfig("192.168.10.2", "nms", "cisco")
-#     myapp.show()
-#         
-#     app.exec_()
-#     
-# if __name__ == "__main__":
-#     main()
-            
-         
+class L3IntfConfig(QWidget, l3intf):
+    def __init__(self, ip, username, password):
+        QWidget.__init__(self)
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.setupUi(self)
+        self.populate_intf_combo()
+        self.populate_nat_combo()
+        self.button_handler()
         
+    def button_handler(self):
+        self.btn_current_config.clicked.connect(self.show_current_config)
+        self.btn_build_config.clicked.connect(self.config_preview)
+        self.btn_close.clicked.connect(self.close_intf_dialog)
+        self.btn_write_config.clicked.connect(self.write_cisco_config)
+    
+    def populate_intf_combo(self):
+        self.cmb_interfaces.clear()
+        output = show_cmd_ssh(self.ip, self.username, self.password, "show ip int brief")
+        intf_list = []
+        for row in output:
+            self.cmb_interfaces.addItem(row.replace("\r","").strip("\n"))
+    
+    def current_config(self):
+        selection = self.cmb_interfaces.currentText()
+        interface = selection.split(' ')[0]
+        current_config_list = []
+        command = "show run interface " + interface
+        int_config = show_cmd_ssh(self.ip, self.username, self.password, command)
+        for line in int_config:
+            current_config_list.append(line.replace("\r","").strip("\n"))
+        return current_config_list
+    
+    def show_current_config(self):
+        current_config = self.current_config()
+        current_config_str = "\n"
+        current_config_str = current_config_str.join(current_config)
+        #
+        #print(current_config)
+        #
+        curr_config_diag = QMessageBox()
+        curr_config_diag.setWindowTitle("Current Configuration ...")
+        curr_config_diag.setText(current_config_str)
+        curr_config_diag.setModal(True)
+        curr_config_diag.addButton(QMessageBox.Ok)
+        curr_config_diag.exec()
+        
+    def populate_nat_combo(self):
+        options = ["", "Inside", "Outside"]
+        for option in options:
+            self.combo_nat.addItem(option)
+            
+    def build_config_cisco(self):
+        selection = self.cmb_interfaces.currentText()
+        interface = selection.split(' ')[0]
+        config_list = ["terminal length 0", "configure terminal"]
+        config_list.append(f"interface {interface}")
+        description = self.edit_l3_desc.text()
+        config_list.append(f"description {description}")
+        l3_ip_add = self.edit_ip_address.text()
+        l3_subnet_mask = self.edit_mask.text()
+        config_list.append(f"ip address {l3_ip_add} {l3_subnet_mask}")
+        ip_helper = self.edit_ip_helper.text()
+        config_list.append(f"ip helper {ip_helper}")
+        acl_in = self.edit_acl_in.text()
+        config_list.append(f"access-class {acl_in} in")
+        acl_out = self.edit_acl_out.text()
+        config_list.append(f"access-class {acl_out} out")
+        nat_operation = self.combo_nat.currentText()
+        if nat_operation == "Inside":
+            config_list.append("ip nat inside")
+        elif nat_operation == "Outside":
+            config_list.append("ip nat outside")     
+        if self.check_shutdown.isChecked():
+            config_list.append("shutdown")
+        else:
+            config_list.append("no shutdown")
+        
+        return config_list
+    
+    def config_preview(self):
+        configs = self.build_config_cisco()
+        config_str = "\n"
+        config_str = config_str.join(configs)
+        #
+        preview_diag = QMessageBox()
+        preview_diag.setWindowTitle("Configuration Preview")
+        preview_diag.setText(config_str)
+        preview_diag.setModal(True)
+        preview_diag.addButton(QMessageBox.Ok)
+        preview_diag.exec()
+        
+    def write_cisco_config(self):
+        self.lbl_config_status.setText("WRITING CONFIGS NOW...")
+        sleep(1)
+        cmd_list = self.build_config_cisco()
+        if config_cmd_ssh(self.ip, self.username, self.password, cmd_list):
+            success_diag = QMessageBox()
+            success_diag.Icon(QMessageBox.Information)
+            success_diag.setWindowTitle("Success")
+            success_diag.setText("Configurations done successfully.")
+            success_diag.exec()
+            self.lbl_config_status.setText("")
+    
+    def close_intf_dialog(self):
+        self.close()
+
+class L3IntfConfigJunos(QWidget, l3intfjunos):
+    def __init__(self, ip, username, password):
+        QWidget.__init__(self)
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.setupUi(self)
+        self.populate_intf_combo()
+        self.button_handler()
+        
+    def button_handler(self):
+        self.btn_current_config.clicked.connect(self.show_current_config)
+#         self.btn_build_config.clicked.connect(self.config_preview)
+        self.btn_close.clicked.connect(self.close_intf_dialog)
+#         self.btn_write_config.clicked.connect(self.write_cisco_config)
+    
+    def populate_intf_combo(self):
+        self.cmb_interfaces.clear()
+        output = show_cmd_ssh(self.ip, self.username, self.password, "show interfaces terse")
+        intf_list = []
+        for row in output:
+            self.cmb_interfaces.addItem(row.replace("\r","").strip("\n"))
+    
+    def current_config(self):
+        selection = self.cmb_interfaces.currentText()
+        interface = selection.split(' ')[0]
+        current_config_list = []
+        command = "show configuration interface " + interface
+        int_config = show_cmd_ssh(self.ip, self.username, self.password, command)
+        for line in int_config:
+            current_config_list.append(line.replace("\r","").strip("\n"))
+        return current_config_list
+    
+    def show_current_config(self):
+        current_config = self.current_config()
+        current_config_str = "\n"
+        current_config_str = current_config_str.join(current_config)
+        #
+        #print(current_config)
+        #
+        curr_config_diag = QMessageBox()
+        curr_config_diag.setWindowTitle("Current Configuration ...")
+        curr_config_diag.setText(current_config_str)
+        curr_config_diag.setModal(True)
+        curr_config_diag.addButton(QMessageBox.Ok)
+        curr_config_diag.exec()      
+
+            
+    def build_config_junos(self):
+        selection = self.cmb_interfaces.currentText()
+        interface = selection.split(' ')[0]
+        config_list = ["terminal length 0", "configure terminal"]
+        config_list.append(f"interface {interface}")
+        description = self.edit_l3_desc.text()
+        config_list.append(f"description {description}")
+        l3_ip_add = self.edit_ip_address.text()
+        l3_subnet_mask = self.edit_mask.text()
+        config_list.append(f"ip address {l3_ip_add} {l3_subnet_mask}")
+        ip_helper = self.edit_ip_helper.text()
+        config_list.append(f"ip helper {ip_helper}")
+        acl_in = self.edit_acl_in.text()
+        config_list.append(f"access-class {acl_in} in")
+        acl_out = self.edit_acl_out.text()
+        config_list.append(f"access-class {acl_out} out")
+        nat_operation = self.combo_nat.currentText()
+        if nat_operation == "Inside":
+            config_list.append("ip nat inside")
+        elif nat_operation == "Outside":
+            config_list.append("ip nat outside")     
+        if self.check_shutdown.isChecked():
+            config_list.append("shutdown")
+        else:
+            config_list.append("no shutdown")
+        
+        return config_list
+    
+    def config_preview(self):
+        configs = self.build_config_cisco()
+        config_str = "\n"
+        config_str = config_str.join(configs)
+        #
+        preview_diag = QMessageBox()
+        preview_diag.setWindowTitle("Configuration Preview")
+        preview_diag.setText(config_str)
+        preview_diag.setModal(True)
+        preview_diag.addButton(QMessageBox.Ok)
+        preview_diag.exec()
+        
+    def write_cisco_config(self):
+        self.lbl_config_status.setText("WRITING CONFIGS NOW...")
+        sleep(1)
+        cmd_list = self.build_config_cisco()
+        if config_cmd_ssh(self.ip, self.username, self.password, cmd_list):
+            success_diag = QMessageBox()
+            success_diag.Icon(QMessageBox.Information)
+            success_diag.setWindowTitle("Success")
+            success_diag.setText("Configurations done successfully.")
+            success_diag.exec()
+            self.lbl_config_status.setText("")
+    
+    def close_intf_dialog(self):
+        self.close() 
+        
+def main():
+    app = QApplication(sys.argv)
+         
+    myapp = L3IntfConfigJunos("192.168.10.31", "nms", "cisco123")
+    myapp.show()
+         
+    app.exec_()
+     
+if __name__ == "__main__":
+    main()
+               
+           
+          
